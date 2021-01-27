3d0480269212ebc231464a2a05696b6ac65383da
pratheekmurthy <pratheekkmurthy516@gmail.com>
2021-01-27 20:04:56 +0530

    loginmodule

--- src/actions/loginAction.js
+++ src/actions/loginAction.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
-import swal from 'sweet-alert'
+import swal from 'sweetalert'
+
+
+const login =() =>{
+        return {type : 'TOGGLE_STATE'}
+}
 
 export const formSubmit =(data,handleRedirect)=>{
     return(dispatch)=>{
@@ -11,11 +16,33 @@ export const formSubmit =(data,handleRedirect)=>{
                 alert(result.message)
             }else{
             handleRedirect()
-            swal("cool", "You have registered successfully!", "success")
+            swal("cool", "You have Registered in successfully!", "success")
         }
         })
         .catch((err)=>{
-            alert(err.message)
+            console.log(err.message)
         })
     }
 }
+
+export const loginformSubmit =(data,handleRedirect)=>{
+    return(dispatch)=>{
+        console.log(data,"i am in gen")
+        axios.post("http://dct-billing-app.herokuapp.com/api/users/login",data)
+        .then((response)=>{
+            const result = response.data
+            if(result.hasOwnProperty('errors')) {
+                alert(result.message)
+            }else{
+                dispatch(login)
+                localStorage.setItem('token',result.token)
+            handleRedirect()
+            swal("cool", "You have loggedIN successfully!", "success")
+        }
+        })
+        .catch((err)=>{
+            console.log(err.message)
+        })
+    }
+
+    }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,59 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+import {loginformSubmit} from '../actions/loginAction'
 
 const Login =(props)=>{
+    const [email,setEmail] = useState("")
+    const [password,setPassword]= useState("")
+    const [formErrors, setFormErrors] = useState({})
+    const errors = {}
+
+    const dispatch = useDispatch()
+
+    const handleEmail =(e)=> setEmail(e.target.value)
+    const handlePassword =(e)=>setPassword(e.target.value)
+    const handleReset =(e)=>{
+        setEmail("")
+        setPassword("")
+    }
+
+    const handleRedirect =()=>{
+        props.history.push("/")
+    }
+
+    const runValidations =()=>{
+        if(email.trim().length === 0){
+            errors.email= "Email cannot be blank"
+        }
+        if(password.trim().length === 0){
+            errors.password = "Password cannot be blank"
+        }
+    }
+
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+        console.log("i am here")
+        runValidations()
+        if(Object.keys(errors).length === 0) {
+            setFormErrors({})
+            const formData = {
+            email :email,
+            password :password
+        }
+        dispatch(loginformSubmit(formData,handleRedirect))
+        
+    }else {
+        console.log('form errors', errors)
+        setFormErrors(errors)
+    }
+}
+
     return(<div>
-        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+            <input type="email" value={email} onChange={handleEmail} placeholder="Enter your email"/>{formErrors.email && <span> { formErrors.email } </span>}<br/>
+            <input type="password" value={password} onChange={handlePassword} placeholder="Enter your Password"/>{formErrors.password && <span> { formErrors.password } </span>}<br/>
+            <button onClick={handleReset}>Reset</button><input type="submit" value="LogIN"/>
+        </form>
     </div>)
 }
 
